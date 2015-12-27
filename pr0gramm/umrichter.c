21fa848b7fc1d2b4dd3d345516cd489750e6fca9
Philipp Hörauf <github@phoerauf.de>
2015-12-27 15:51:07 +0100

    PI Regler leicht angepasst, dazu eine Steuermöglichkeit über die Konsole integriert, x stoppt den Motor nun sofort.

--- pr0gramm/umrichter.c
+++ pr0gramm/umrichter.c
@@ -1,6 +1,6 @@
 /*
  *    Filename: umrichter.c
- *     Version: 0.0.1
+ *     Version: 0.0.3
  * Description: Ansteuerung eines A4960-Drehstromwandlers
  *     License: GPLv3 or later
  *     Depends: global.h, io.h, interrupt.h
@@ -42,10 +42,10 @@
 // Taktfrequenz = fCPU, Ausgabefrequenz 15,625kHz
 // clear OC1A on compare match, top=1023
 void timerInit (void) {
-	// TimerCounter 1: FastPWM
+	// TimerCounter 1: FastPWM, 10 Bit, no Prescaler
 	TCCR1A = 1<<COM1A1 | 1<<WGM11 | 1<<WGM10;
 	TCCR1B = 1<<WGM12 | 1<<CS10;
-	OCR1A = 100; // Startwert 10%
+	OCR1A = 100; // Startwert 10% Power
 	
 	// TimerCounter 2: CTC Mode für CRON (compareMatch alle 10ms)
 	TCCR2 = 1<<WGM21 | 1<<CS22 | 1<<CS21 | 1<<CS20;
@@ -58,6 +58,7 @@ void isrInit (void) {
 	// INT1 und INT0 rising edge
 	MCUCR = 1<<ISC01 | 1<<ISC00 | 1<<ISC11 | 1<<ISC10;
 	GICR = 1<<INT1 | 1<<INT0;
+	UCSRB |= 1<<RXCIE;
 }
 
 void adcInit(void) {
@@ -111,6 +112,7 @@ uint16_t a4960ReadStatus (void) {
 	return a4960Com(0b1100, 0);
 }
 
+volatile int8_t motorstatus = 0; // 0 heißt OFF, 1 RECHTS, -1 LINKS
 void a4960Init (void) {
 	sbi(PORTD, PD4); // aktiviere den a4960
 	delayms(10);
@@ -124,12 +126,19 @@ void a4960Init (void) {
 	a4960Com (0b1111, BW2 | BW1 | RSC);
 }
 
-void a4960MotorOn (void) {
-	a4960Com (0b1111, BW2 | RSC | RUN);
+void a4960MotorOn (int8_t drehrichtung) {
+	if (drehrichtung > 0) {
+		a4960Com (0b1111, BW2 | RSC | RUN);
+		motorstatus = 1;
+	} else if (drehrichtung < 0) {
+		a4960Com (0b1111, BW2 | RSC | RUN | DIR);
+		motorstatus = -1;
+	}
 }
 
 void a4960MotorOff (void) {
 	a4960Com (0b1111, BW2 | RSC);
+	motorstatus = 0;
 }
 
 
@@ -139,28 +148,43 @@ void a4960MotorOff (void) {
 // INT0 wird durch eine steigende Flanke vom TACHO-Ausgang getriggert.
 volatile uint16_t anzahlWellen = 0;
 ISR (INT0_vect, ISR_BLOCK) {
+	ERROR(1);
 	anzahlWellen ++;
+	ERROR(0);
 }
 
 // ERROR Handler
 ISR (INT1_vect, ISR_BLOCK) {
-	ERROR(1);
+// 	ERROR(1);
+}
+
+ISR (USART_RXC_vect, ISR_BLOCK) {
+	char empfangen = UDR;
+	uartTx(empfangen); // remote echo
+	if (empfangen == 'x') {
+		a4960MotorOff();
+	} else if (empfangen == 's') {
+		a4960MotorOn(1);
+	}
+	
 }
 
 // CRON und Regelung
-uint16_t frequenz = 0, sollfrequenz = 1200; // natürlich 50Hz.
-float kp=0.01, ki=0.005;
+uint16_t tacho = 0, sollfrequenz = 1200; // Tachofrequenz...
+float kp=0.05, ki=0.01;
 volatile uint16_t eingangsspannung = 0, logikspannung = 0;
-// float UinConversionFaktor = (1.0/11.0);
-// float UlogikConversionFaktor = (14.0/39.0);
+#define IMAX 100
+#define PWMMAX 1023 // Vollgas für 10Bit PWM
 #define VREF 2560 // in mV
+
 ISR (TIMER2_COMP_vect, ISR_BLOCK) {
+	ERROR(1);
 	static uint8_t systick=0;
 	static uint8_t adcKanal=0; // ch0 = Vin, ch1 = +5V
 	systick ++;
 	if (systick == 10) { // 100ms vergangen, regelzeit!
 		systick = 0;
-		frequenz = anzahlWellen*10;
+		tacho = anzahlWellen*10;
 		anzahlWellen = 0;
 		
 		uint16_t adcWert = ADC;
@@ -178,12 +202,22 @@ ISR (TIMER2_COMP_vect, ISR_BLOCK) {
 		
 		adcStart(adcKanal);
 		
+		// Regler: Konzept PI, D folgt
 		// Vorsteuerung: PWM-Grad von 100 entspricht 1kHz
 		
-		float ek = (float)sollfrequenz - (float)frequenz;
+		float ek = (float)sollfrequenz - (float)tacho;
 		static float yk = 0;
-		yk += (ki * ek);
-		int out = (100 + yk + (kp*ek));
+		
+// 		RampUp-Schutz für I-Anteil
+		if ((yk + (ki * ek)) < IMAX) {
+			yk += (ki * ek);
+		}
+		int out = 0;
+		if (motorstatus) {
+			out = (100 + yk + (kp*ek));
+		} else {
+			out = 0;
+		}
 		
 #ifdef DEBUG
 		char buf[50];
@@ -192,14 +226,15 @@ ISR (TIMER2_COMP_vect, ISR_BLOCK) {
 #endif
 		
 		// Setze PWM-Grad
-		if (out > 300) {
-			OCR1A = 300;
+		if (out > PWMMAX) {
+			OCR1A = PWMMAX;
 		} else if (out < 0) {
 			OCR1A = 0;
 		} else {
 			OCR1A = (out);
 		}
 	}
+	ERROR(0);
 }
 
 
@@ -233,10 +268,10 @@ int main(void) {
 	
 	// Aktiviere Programmmodule
 	timerInit();
-	isrInit();
-	spiInit();
 	uartInit();
+	spiInit();
 	adcInit();
+	isrInit();
 	
 	delayms(100);
 	uartTxNewline();
@@ -246,19 +281,27 @@ int main(void) {
 // 	uint16_t tempvar = 0;
 	
 	a4960Init();
-	delayms(10);
-	a4960MotorOn();
-	uartTxStrln("aktiviere Motor");
-	ON(1);
+	uartTxStrln("aktiviere Motor mit Druck auf s...");
 	
 	
 	sei(); // und es seien Interrupts :D
 	
-	
+	uint8_t i = 0;
 	while(1) {
-		sprintf(buf, "Frequenz(ist): %u Hz, Motorspannung: %#2.2u mV, Logikspannung: %#2.2u mV       \r", frequenz, eingangsspannung, logikspannung);
+		sprintf(buf, "F(soll): %u Hz, F(ist): %u Hz, UMotor: %#2.2u mV, ULogik: %#2.2u mV       \r", sollfrequenz, tacho, eingangsspannung, logikspannung);
 		uartTxStr(buf);
 		delayms(250);
+		if (motorstatus) {
+			i++;
+		}
+		if (i == 40) {
+			sollfrequenz = 2000;
+			ON(1);
+		} else if (i == 80) {
+			sollfrequenz = 500;
+			ON(0);
+			i=0;
+		}
 	}
 	return 0;
 }
